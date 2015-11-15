0c716edfbb6c07222f765abaaa7eeee1775bae05
unonu <unonuorea@gmail.com>
2015-11-15 12:19:52 -0500

    add the mic stuff (double whoops)

---
+++ app/src/main/java/com/rcos/unonu/tact/AudioVolume.java
@@ -0,0 +1,35 @@
+package com.rcos.unonu.tact;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+
+/**
+ * Created by alwin on 11/14/15.
+ */
+public class AudioVolume {
+    private AudioRecord ar;
+    private int minSize;
+
+    public void start() {
+        minSize = AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        ar = new AudioRecord(MediaRecorder.AudioSource.MIC, 8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, minSize);
+        ar.startRecording();
+    }
+
+    public void stop() {
+        ar.stop();
+        ar.release();
+    }
+
+    public int getAmplitude() {
+        short[] buffer = new short[minSize];
+        ar.read(buffer, 0, minSize);
+        int max = 0;
+        for (short s : buffer)
+            if (Math.abs(s) > max)
+                max = Math.abs(s);
+        return max;
+    }
+}
