13a7e8e224af1bdee26eacdeab5bc3da87071c06
unonu <unonuorea@gmail.com>
2015-11-14 21:19:50 -0500

    saving

--- app/src/main/java/com/rcos/unonu/tact/AlarmReceiver.java
+++ app/src/main/java/com/rcos/unonu/tact/AlarmReceiver.java
@@ -1,10 +1,19 @@
 package com.rcos.unonu.tact;
 
-import android.content.BroadcastReceiver;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.content.WakefulBroadcastReceiver;
 
-public class AlarmReceiver extends BroadcastReceiver {
+import java.util.Calendar;
+
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+    private AlarmManager alarmManager;
+    private PendingIntent alarmIntent;
+
     public AlarmReceiver() {
     }
 
@@ -13,6 +22,32 @@ public class AlarmReceiver extends BroadcastReceiver {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         Intent service = new Intent(context, SchedulingService.class);
-        //startWakefulService(context, service);
+        startWakefulService(context, service);
+
+    }
+
+    public void setAlarm(Context context) {
+        alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 8);
+        calendar.set(Calendar.MINUTE, 30);
+
+        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                calendar.getTimeInMillis(),
+                5*60*1000,
+                alarmIntent);
+
+        // Enable {@code SampleBootReceiver} to automatically restart the alarm when the
+        // device is rebooted.
+        ComponentName receiver = new ComponentName(context, BootReceiver.class);
+        PackageManager pm = context.getPackageManager();
+
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
     }
 }
